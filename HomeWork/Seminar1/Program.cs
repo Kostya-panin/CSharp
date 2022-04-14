07a92d7e76606d50130c1f5f2615ae16d50c1b80
konstantin <panin.kostya@outlook.com>
2022-04-14 14:34:17 +0500

    added task 2-4

--- HomeWork/Seminar1/Program.cs
+++ HomeWork/Seminar1/Program.cs
@@ -27,29 +27,47 @@ Console.Write("minimum number:"+ min);
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
-int max=0;
+/*
 Console.WriteLine("Input first number:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second number:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input third number:");
 int number3 = Convert.ToInt32(Console.ReadLine());
-if (number1 == number2 & number2 == number3)
-    Console.Write("numbers equal");
+int max=number1;
+if (number1 == number2 & number2 == number3) Console.Write("numbers equal");
     else
-        if (number1 > (number2 & number3))
-    max=number1;
-        if (number2 > (number1 & number3))
-    max=number2;
-        if (number3 > (number2 & number1))
-    max=number3;
-Console.Write("Maximum number: "+ max);
+        {
+        if (number1 > max) max=number1;
+        if (number2 > max) max=number2;
+        if (number3 > max) max=number3;
+        Console.Write("Maximum number: "+ max);
+        }
+  */      
 //Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 //4 -> да
 //-3 -> нет
 //7 -> нет
-
+/*
+Console.WriteLine("Input integer number:");
+int number = Convert.ToDouble(Console.ReadLine());
+int div = number % 2;
+if (div==0) Console.WriteLine(number + "-"+"even number");
+else
+Console.WriteLine(number + "-"+"uneven number");
+Console.WriteLine(number % 2);
+*/
 //Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+/*
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 0;
+while (current<=number)
+{
+    current=current + 2;
+ Console.Write((current) +" ");
+}
+*/
 
