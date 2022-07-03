4d68a814db7ea17ee680cb92e2b0510b32292aac
konstantin <panin.kostya@outlook.com>
2022-07-03 14:05:41 +0500

    update program

--- HomeWork/Test/Program.cs
+++ HomeWork/Test/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "rmvfk", "7", "gjnv", ":-)", "Wea", "rew" };
+string[] arrayEdit(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int i = 0;
+    int j = 0;
+    int maxLength = 3;
+    while (i < array.Length)
+    {
+        if (array[i].Length <= maxLength)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+        i++;
+    }
+    Array.Resize(ref newArray, j);
+    return newArray;
+}
+void printArray(string [] array)
+{
+Console.Write("[");
+for (int i = 0;i < array.Length;i++)
+{
+    Console.Write(array[i]);
+    if (i<(array.Length-1))Console.Write(" ");
+}
+Console.Write("]");
+}
+string [] newarray=arrayEdit(array);
+printArray(array);
+Console.Write(" -> ");
+printArray(newarray);
